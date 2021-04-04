db391b10b8f91b23b86f8cea70651911e92a3715
unknown <pedroalvesvalentim@gmail.com>
2021-04-04 04:51:33 -0300

    Moved automata manipulation functions to Automaton class as methods

--- Automaton.py
+++ Automaton.py
@@ -1,3 +1,6 @@
+# Epsilon
+E = "_e"
+
 class Automaton:
    
    def __init__(self, sigma, Q, delta, ini, F):
@@ -21,3 +24,107 @@ class Automaton:
       print("Initial state:", self.ini)
       print("Final states:", self.F)
       return ""
+
+   def concat_automata(self, a):
+      # Combina sigmas
+      sigma = self.sigma | a.sigma
+      # Renomeia estados em Q e F
+      for i, state in enumerate(self.Q):
+         self.Q[i] = state+"1"
+      self.ini += "1"
+      for i, state in enumerate(self.F):
+         self.F[i] = state+"1"
+      for i, state in enumerate(a.Q):
+         a.Q[i] = state+"2"
+      a.ini += "2"
+      for i, state in enumerate(a.F):
+         a.F[i] = state+"2"
+      # Combina estados
+      Q = self.Q + a.Q
+      # Renomeia estados em delta, renomeia estados em transições e combina deltas
+      delta = {}
+      for state in self.delta:
+         delta[state+"1"] = []
+         for i, trans in enumerate(self.delta[state]):
+            new_trans = (trans[0], trans[1]+"1")
+            self.delta[state][i] = new_trans
+            delta[state+"1"].append(new_trans)
+      for state in a.delta:
+         delta[state+"2"] = []
+         for i, trans in enumerate(a.delta[state]):
+            new_trans = (trans[0], trans[1]+"2")
+            a.delta[state][i] = new_trans
+            delta[state+"2"].append(new_trans)
+      # Adiciona transição nova
+      for i, state in enumerate(self.F): 
+         delta[state].append((E, a.ini))
+      return Automaton(sigma, Q, delta, self.ini, a.F)
+
+   def sum_automata(self, a):
+      # Combina sigmas
+      sigma = self.sigma | a.sigma
+      # Renomeia estados em Q e F
+      for i, state in enumerate(self.Q):
+         self.Q[i] = state+"1"
+      self.ini += "1"
+      for i, state in enumerate(self.F):
+         self.F[i] = state+"1"
+      for i, state in enumerate(a.Q):
+         a.Q[i] = state+"2"
+      a.ini += "2"
+      for i, state in enumerate(a.F):
+         a.F[i] = state+"2"
+      # Combina estados
+      Q = self.Q + a.Q
+      # Renomeia estados em delta, renomeia estados em transições e combina deltas
+      delta = {}
+      for state in self.delta:
+         delta[state+"1"] = []
+         for i, trans in enumerate(self.delta[state]):
+            new_trans = (trans[0], trans[1]+"1")
+            self.delta[state][i] = new_trans
+            delta[state+"1"].append(new_trans)
+      for state in a.delta:
+         delta[state+"2"] = []
+         for i, trans in enumerate(a.delta[state]):
+            new_trans = (trans[0], trans[1]+"2")
+            a.delta[state][i] = new_trans
+            delta[state+"2"].append(new_trans)
+      # Adiciona estados novos
+      Q.append('q0')
+      Q.append('qf')
+      delta['qf'] = []
+      # Adiciona transições novas
+      delta['q0'] = [(E, self.ini), (E, a.ini)]
+      for i, state in enumerate(self.F): 
+         delta[state].append((E, 'qf'))
+      for i, state in enumerate(a.F): 
+         delta[state].append((E, 'qf'))
+      return Automaton(sigma, Q, delta, 'q0', ['qf'])
+
+   def multiply_automaton(self):
+      # Renomeia estados em Q, F e delta
+      for i, state in enumerate(self.Q):
+         self.Q[i] = state+"1"
+      self.ini += "1"
+      for i, state in enumerate(self.F):
+         self.F[i] = state+"1"
+      delta = {}
+      for state in self.delta:
+         delta[state+"1"] = []
+         for i, trans in enumerate(self.delta[state]):
+            new_trans = (trans[0], trans[1]+"1")
+            delta[state+"1"].append(new_trans)
+      self.delta = delta
+      # Adiciona estados novos
+      self.Q.append('q0')
+      self.Q.append('qf')
+      self.delta['qf'] = []
+      # Adiciona transições novas
+      self.delta['q0'] = [(E, self.ini), (E, 'qf')]
+      for i, state in enumerate(self.F): 
+         self.delta[state].append((E, 'qf'))
+         self.delta[state].append((E, self.ini))
+      self.ini = 'q0'
+      self.F = ['qf']
+      return self
--- matcher.py
+++ matcher.py
@@ -1,116 +1,9 @@
 from collections import deque
 from Automaton import Automaton 
 
-# Epsilon
-E = "_e"
-
 def min_automaton(s):
    d = { 'q0': [(s, 'q1')], 'q1': [] }
    return Automaton({s}, ['q0','q1'], d, 'q0', ['q1'])
-
-def concat_automata(a1, a2):
-   # Combina sigmas
-   sigma = a1.sigma | a2.sigma
-   # Renomeia estados em Q e F
-   for i, state in enumerate(a1.Q):
-      a1.Q[i] = state+"1"
-   a1.ini += "1"
-   for i, state in enumerate(a1.F):
-      a1.F[i] = state+"1"
-   for i, state in enumerate(a2.Q):
-      a2.Q[i] = state+"2"
-   a2.ini += "2"
-   for i, state in enumerate(a2.F):
-      a2.F[i] = state+"2"
-   # Combina estados
-   Q = a1.Q + a2.Q
-   # Renomeia estados em delta, renomeia estados em transições e combina deltas
-   delta = {}
-   for state in a1.delta:
-      delta[state+"1"] = []
-      for i, trans in enumerate(a1.delta[state]):
-         new_trans = (trans[0], trans[1]+"1")
-         a1.delta[state][i] = new_trans
-         delta[state+"1"].append(new_trans)
-   for state in a2.delta:
-      delta[state+"2"] = []
-      for i, trans in enumerate(a2.delta[state]):
-         new_trans = (trans[0], trans[1]+"2")
-         a2.delta[state][i] = new_trans
-         delta[state+"2"].append(new_trans)
-   # Adiciona transição nova
-   for i, state in enumerate(a1.F): 
-      delta[state].append((E, a2.ini))
-   return Automaton(sigma, Q, delta, a1.ini, a2.F)
-
-def sum_automata(a1, a2):
-   # Combina sigmas
-   sigma = a1.sigma | a2.sigma
-   # Renomeia estados em Q e F
-   for i, state in enumerate(a1.Q):
-      a1.Q[i] = state+"1"
-   a1.ini += "1"
-   for i, state in enumerate(a1.F):
-      a1.F[i] = state+"1"
-   for i, state in enumerate(a2.Q):
-      a2.Q[i] = state+"2"
-   a2.ini += "2"
-   for i, state in enumerate(a2.F):
-      a2.F[i] = state+"2"
-   # Combina estados
-   Q = a1.Q + a2.Q
-   # Renomeia estados em delta, renomeia estados em transições e combina deltas
-   delta = {}
-   for state in a1.delta:
-      delta[state+"1"] = []
-      for i, trans in enumerate(a1.delta[state]):
-         new_trans = (trans[0], trans[1]+"1")
-         a1.delta[state][i] = new_trans
-         delta[state+"1"].append(new_trans)
-   for state in a2.delta:
-      delta[state+"2"] = []
-      for i, trans in enumerate(a2.delta[state]):
-         new_trans = (trans[0], trans[1]+"2")
-         a2.delta[state][i] = new_trans
-         delta[state+"2"].append(new_trans)
-   # Adiciona estados novos
-   Q.append('q0')
-   Q.append('qf')
-   delta['qf'] = []
-   # Adiciona transições novas
-   delta['q0'] = [(E, a1.ini), (E, a2.ini)]
-   for i, state in enumerate(a1.F): 
-      delta[state].append((E, 'qf'))
-   for i, state in enumerate(a2.F): 
-      delta[state].append((E, 'qf'))
-   return Automaton(sigma, Q, delta, 'q0', ['qf'])
-
-def multiply_automaton(a):
-   # Renomeia estados em Q, F e delta
-   for i, state in enumerate(a.Q):
-      a.Q[i] = state+"1"
-   a.ini += "1"
-   for i, state in enumerate(a.F):
-      a.F[i] = state+"1"
-   delta = {}
-   for state in a.delta:
-      delta[state+"1"] = []
-      for i, trans in enumerate(a.delta[state]):
-         new_trans = (trans[0], trans[1]+"1")
-         delta[state+"1"].append(new_trans)
-   a.delta = delta
-   # Adiciona estados novos
-   a.Q.append('q0')
-   a.Q.append('qf')
-   a.delta['qf'] = []
-   # Adiciona transições novas
-   a.delta['q0'] = [(E, a.ini), (E, 'qf')]
-   for i, state in enumerate(a.F): 
-      a.delta[state].append((E, 'qf'))
-      a.delta[state].append((E, a.ini))
-   a.ini = 'q0'
-   a.F = ['qf']
-   return a
 
 op = ['.', '+', '*']
 
@@ -120,16 +13,16 @@ def parse(tokens, automaton=None):
       if token=='.':
          t1, a1 = parse(tokens, automaton)
          t2, a2 = parse(tokens, automaton)
-         aF = concat_automata(a1, a2)
+         aF = a1.concat_automata(a2)
          return t1+t2, aF
       elif token=='+':
          t1, a1 = parse(tokens, automaton)
          t2, a2 = parse(tokens, automaton)
-         aF = sum_automata(a1, a2)
+         aF = a1.sum_automata(a2)
          return "("+t1+"+"+t2+")", aF
       elif token=='*':
          t, a = parse(tokens, automaton)
-         a = multiply_automaton(a)
+         a.multiply_automaton()
          return t+"*", a
    else:
       a = min_automaton(token)
