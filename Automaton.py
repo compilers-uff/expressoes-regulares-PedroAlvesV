d53192fb2c646e7ece16e9e4722eb4e522cb323c
unknown <pedroalvesvalentim@gmail.com>
2021-04-11 04:14:17 -0300

    Added utils functions module

--- Automaton.py
+++ Automaton.py
@@ -1,3 +1,5 @@
+import utils
+
 # Epsilon
 E = "_e"
 
@@ -12,17 +14,11 @@ class Automaton:
       self.ini = ini
       self.F = F
    
-   def print_delta(self):
-      for state in self.delta:
-         print(state)
-         for trans in self.delta[state]:
-            print('\t', trans)
-   
    def __str__(self):
       print("Sigma:", self.sigma)
       print("Q:", self.Q)
       print("Delta:")
-      self.print_delta()
+      utils.print_delta(self.delta)
       print("Initial state:", self.ini)
       print("Final states:", self.F)
       if self.e_closure_table:
